1cb951248c64101f4904e3f3a279d236d4c899d3
ShivadeT0T <maxkolomainen@hotmail.com>
2023-12-27 01:46:42 +0200

    Made preparations for testing

---
+++ Project/Backend/app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const connectDB = require("./config/db");
+const express = require("express");
+const cors = require("cors");
+const { errorMiddleware } = require("./middleware/errorMiddleware");
+const { notFoundMiddleware } = require("./middleware/notFoundMiddleware");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+connectDB();
+
+// Body Parser Middleware
+app.use(cors());
+app.use(express.json());
+
+// Recruiters API Routes
+app.use("/api/recruiters", require("./routes/recruitersRoutes"));
+
+// Artists API Routes
+app.use("/api/artists", require("./routes/artistsRoutes"));
+
+// Projects API Routes
+app.use("/api/projects", require("./routes/projectsRoutes"));
+
+// Jobs API Routes
+app.use("/api/jobs", require("./routes/jobsRoutes"));
+
+app.use;
+
+app.use(errorMiddleware);
+app.use(notFoundMiddleware);
+
+module.exports = app;
--- Project/Backend/server.js
+++ Project/Backend/server.js
@@ -1,34 +1,9 @@
-require("dotenv").config();
-const connectDB = require("./config/db");
-const express = require('express');
-const cors = require("cors");
-const { errorMiddleware } = require('./middleware/errorMiddleware');
-const { notFoundMiddleware } = require('./middleware/notFoundMiddleware');
+const app = require("./app");
+const http = require("http");
+const config = require("./config/config");
 
-const app = express();
+const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3001;
-connectDB();
-
-// Body Parser Middleware
-app.use(cors());
-app.use(express.json());
-
-// Recruiters API Routes
-app.use('/api/recruiters', require('./routes/recruitersRoutes'));
-
-// Artists API Routes
-app.use('/api/artists', require('./routes/artistsRoutes'));
-
-// Projects API Routes
-app.use('/api/projects', require('./routes/projectsRoutes'));
-
-// Jobs API Routes
-app.use('/api/jobs', require('./routes/jobsRoutes'));
-
-app.use
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.use(errorMiddleware);
-app.use(notFoundMiddleware);
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
